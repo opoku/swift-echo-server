2b1b66cf14ffe25d28e71068ecb8a1c07ef9bdf2
Osei Poku <opoku@opkoder.com>
2015-07-23 22:30:09 -0700

    Initial Commit

---
+++ HTTPServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HTTPServer
+//
+//  Created by Osei Poku on 7/23/15.
+//  Copyright © 2015 OPKoder. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
